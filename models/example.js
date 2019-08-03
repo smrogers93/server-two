72f4207566de405b0be3018d13bd33858075e38a
Andrew V <red1000@rocketmail.com>
2019-08-03 12:32:39 -0400

    adding in table credentials for example.js and adding sequelize functions

--- models/example.js
+++ models/example.js
@@ -1,7 +1,45 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var SharkAttacks = sequelize.define("SharkAttacks", {
+    date: {
+      type: DataTypes.STRING,
+      defaultValue: "N/A"
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      defaultValue: "N/A"
+    },
+    type: {
+      type: DataTypes.STRING,
+      defaultValue: "N/A"
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    area: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    activity: {
+      type: DataTypes.STRING,
+      defaultValue: "No information"
+    },
+    species: {
+      type: DataTypes.STRING,
+      defaultValue: "N/A"
+    },
+    hasPDF: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: "FALSE"
+    },
+    pdf: {
+      type: DataTypes.STRING,
+      defaultValue: "No PDF available."
+    }
   });
-  return Example;
-};
+  return SharkAttacks;
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,22 +2,68 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/attacks", function(req, res) {
+    db.SharkAttacks.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
+  //get specific shark attack by year
+  app.get("/api/attacks/:year", function(req, res) {
+    db.SharkAttacks.findAll({
+      where: {year = req.params.year}
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  //get specific shark attack by country
+  app.get("/api/attack/:country/:area?", function(req, res) {
+    var queries = {
+      country: req.params.country,
+      area: undefined
+    }
+    if(req.params.area){
+      queries.area = req.params.area;
+    } else {
+      delete queries.area;
+    }
+    db.SharkAttacks.findAll({
+      where: queries
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  //get specific shark attack by country and area
+  app.get("/api/attack/:country/:area/:location?", function(req, res) {
+    var queries = {
+      country: req.params.country,
+      area: req.params.area,
+      location: undefined
+    }
+    if(req.params.location){
+      queries.location = req.params.location;
+    } else {
+      delete queries.location;
+    }
+    db.SharkAttacks.findAll({
+      where: queries
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/attacks", function(req, res) {
+    db.SharkAttacks.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.SharkAttacks.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });

69ac671dda0409c81420cfb26d3143c94623d7d8
Stephen Rogers <49245604+smrogers93@users.noreply.github.com>
2019-08-03 13:17:20 -0400

    Update and rename config.json to config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-{
+module.exports = {
   "development": {
     "username": "root",
-    "password": "rootroot",
+    "password": process.env.mysqlPassword,
     "database": "exampledb",
     "host": "localhost",
     "dialect": "mysql"

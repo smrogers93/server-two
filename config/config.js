59f0114882cf6aa45d701f5c19a5109100d36987
Stephen Rogers <49245604+smrogers93@users.noreply.github.com>
2019-08-03 13:49:46 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "testdb",
     "host": "localhost",
     "dialect": "mysql",
